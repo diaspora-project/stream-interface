1f4e318db651832ed959ef026bfbfac0e71b4302
valhayot <valeriehayot@gmail.com>
2025-06-16 13:58:14 -0500

    Rough implementation of shared interface

---
+++ streaming/base.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Streaming(ABC):
+    """
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+    @abstractmethod
+    def producer(self):
+        pass
+
+    @abstractmethod
+    def consumer(self):
+        pass
+
+
+class Producer(ABC):
+    """_summary_
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+    @abstractmethod
+    def send():
+        pass
+
+    @abstractmethod
+    def flush():
+        pass
+
+
+class Consumer(ABC):
+    """
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+    @abstractmethod
+    def __iter__(self):
+        pass
+
+    @abstractmethod
+    def __next__(self):
+        pass
+
+    @abstractmethod
+    def close():
+        pass
